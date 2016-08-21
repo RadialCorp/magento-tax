ad273ddef580b4bec470ac39b66240c36dfca65c
Robert Krule <rkrule@radial.com>
2016-08-21 11:57:18 -0400

    imported eb2cgiftwrap module

---
+++ src/app/code/community/Radial/Eb2cGiftwrap/Model/Config.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright (c) 2013-2014 eBay Enterprise, Inc.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @copyright   Copyright (c) 2013-2014 eBay Enterprise, Inc. (http://www.radial.com/)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class Radial_Eb2cGiftwrap_Model_Config extends Radial_Eb2cCore_Model_Config_Abstract
+{
+    protected $_configPaths = array(
+        'dummy_tax_class' => 'eb2cgiftwrap/dummy/tax_class',
+        'dummy_base_price' => 'eb2cgiftwrap/dummy/base_price',
+        'dummy_image' => 'eb2cgiftwrap/dummy/image',
+        'dummy_status' => 'eb2cgiftwrap/dummy/status',
+    );
+}
